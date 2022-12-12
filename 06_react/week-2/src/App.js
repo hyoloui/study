debe7c390a1c101cdf2a7bdb80756f1f0020fca2
hyoloui <tmdgy9272@gmail.com>
2022-12-12 12:40:12 +0900

    redux 세팅

--- 06_react/week-2/src/App.js
+++ 06_react/week-2/src/App.js
@@ -1,47 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 import './App.css';
+import StyledCompo from "./component/StyledCompo";
 import Update from "./component/Update";
 import UseEffect from "./component/UseEffect"
 
 
 const App = () => {
 
-  // 1. styled-components를 만들었습니다.
-  const StBox = styled.div`
-    width: 100px;
-    height: 100px;
-    border: 1px solid ${(props) => (props.borderColor)}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다. 
-    margin: 20px;
-  `;
-  const boxList = ["red", "green", "blue"];
-  
-  // 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
-  const getBoxName = (color) => {
-    switch (color) {
-      case "red":
-        return "빨간 박스";
-      case "green":
-        return "초록 박스";
-      case "blue":
-        return "파란 박스";
-      default:
-        return "검정 박스";
-    }
-  };
-
-
   return (
     <>
-      <div className="App">
-        {boxList.map((box) => (
-          <StBox borderColor={box}>{getBoxName(box)}</StBox>
-        ))}
+      <div id="component">
+        <StyledCompo/>
+
+        <Update />
+
+        <UseEffect/>
       </div>
 
-      <Update />
-
-      <UseEffect/>
+      
     </>
   );
 };
--- 06_react/week-2/src/component/UseEffect.jsx
+++ 06_react/week-2/src/component/UseEffect.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [value, setValue] = useState("");
+    const [value, setValue] = useState("");
 
-  useEffect(() => {
-    console.log("hello useEffect");
-  });
+    useEffect(() => {
+        console.log("hello useEffect");
+    }, []); // 비어있는 의존성 배열
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-    </div>
-  );
-}
+    return (
+        <div>
+            <input
+                type="text"
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value);
+                }}
+            />
+        </div>
+    );
+};
 
 export default App;
