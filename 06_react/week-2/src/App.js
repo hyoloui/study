f3b14dc2662b462cd5d698d58176c95355190735
hyoloui <tmdgy9272@gmail.com>
2022-12-12 20:22:30 +0900

    리액트 숙련 1-7 까지

--- 06_react/week-2/src/App.js
+++ 06_react/week-2/src/App.js
@@ -1,25 +1,44 @@
 import React from "react";
-import './App.css';
-import StyledCompo from "./component/StyledCompo";
-import Update from "./component/Update";
-import UseEffect from "./component/UseEffect"
-
-
+import "./App.css";
+// import StyledCompo from "./component/StyledCompo";
+// import Update from "./component/Update";
+// import UseEffect from "./component/UseEffect"
+import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
+import { plusOne, minusOne } from "./redux/modules/counter";
 const App = () => {
+    const dispatch = useDispatch();
+    const state = useSelector((state)=>state)
+    const number = useSelector((state)=>state.counter.number)
+    console.log(number)
+    
+    return (
+        <>
+            <div id="component">
+                {/* <StyledCompo/> */}
 
-  return (
-    <>
-      <div id="component">
-        <StyledCompo/>
+                {/* <Update /> */}
 
-        <Update />
+                {/* <UseEffect/> */}
+            </div>
 
-        <UseEffect/>
-      </div>
-
-      
-    </>
-  );
+            {number}
+            <button
+                onClick={() => {
+                    dispatch(plusOne());
+                }}
+            >
+                +1
+                
+            </button>
+            <button
+                onClick={() => {
+                    dispatch(minusOne());
+                }}
+            >
+                -1
+            </button>
+        </>
+    );
 };
 
 export default App;
