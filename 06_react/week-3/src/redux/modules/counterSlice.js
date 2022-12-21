53a5f0ef2bf816482e42cfb5ee045c962c173a89
hyoloui <tmdgy9272@gmail.com>
2022-12-21 20:24:47 +0900

    ✅ Thunk, axios

--- 06_react/week-3/src/redux/modules/counterSlice.js
+++ 06_react/week-3/src/redux/modules/counterSlice.js
@@ -1,6 +1,18 @@
 // src/redux/modules/counterSlice.js
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const __addNumber = createAsyncThunk(
+    // 첫번째 인자 : action value
+    "addNumber",
+
+    // 두번째 인자 : 콜백함수
+    (payload, thunkAPI) => {
+        setTimeout(() => {
+            thunkAPI.dispatch(addNumber(payload));
+        }, 3000);
+    }
+);
 
 const initialState = {
     number: 0,
@@ -20,7 +32,5 @@ const counterSlice = createSlice({
     },
 });
 
-// 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
 export const { addNumber, minusNumber } = counterSlice.actions;
-// reducer 는 configStore에 등록하기 위해 export default 합니다.
 export default counterSlice.reducer;
